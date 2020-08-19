855a29a50a2962eeb10ea048cc59d0f2025ea472
Sean Chen <skypemaster007@gmail.com>
2020-08-19 14:56:57 -0700

    Add safer Q20

--- src/Remove.hs
+++ src/Remove.hs
@@ -2,8 +2,16 @@ module Remove (removeAt) where
 
 -- Remove the Kth element from a list 
 
-removeAt :: Int -> [a] -> (a, [a])
+--removeAt :: Int -> [a] -> (a, [a])
+--
+--removeAt k xs = (xs !! (k - 1), remaining)
+--    where remaining = take (k - 1) xs ++ drop k xs
 
-removeAt k xs = (xs !! (k - 1), remaining)
-    where remaining = take (k - 1) xs ++ drop k xs
+-- Safer version 
+removeAt :: Int -> [a] -> (Maybe a, [a])
+
+removeAt k xs
+    | k > length xs = (Nothing, xs)
+    | otherwise = (Just (xs !! (k - 1)), remaining)
+        where remaining = take (k - 1) xs ++ drop k xs
 
